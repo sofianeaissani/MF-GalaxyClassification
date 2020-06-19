f16598933d31eebee42a9b5e471432c7f0e454c7
sofianeaissani <sofiane.aissani@hotmail.com>
2020-06-19 15:45:58 +0200

    verifying flux format

--- thresholdDisplay.py
+++ thresholdDisplay.py
@@ -1,48 +1,73 @@
 import sys
 import matplotlib.pyplot as plt
 from libs.imProcess import *
+import argparse
 import numpy as np
 from scipy import ndimage, misc
 import imageio
 
-a = get_image("data/dataset1_z075-100_M214/0005_149p971429482882_1p6524724303036218_acs_I_mosaic_30mas_sci.fits")[0]
-NUMAX = np.max(a)
-NUMIN = np.min(a)
-print("min :", NUMIN, "max :", NUMAX)
 
-b = a.copy()
-c = a.copy()
-d = a.copy()
-e = a.copy()
+def main(myFile):
 
-a_ = a >= (NUMAX-NUMIN)/6
-b = b >= 2*(NUMAX-NUMIN)/6
-c = c >= 3*(NUMAX-NUMIN)/6
-d = d >= 4*(NUMAX-NUMIN)/6
-e = e >= 5*(NUMAX-NUMIN)/6
+    global args
 
-### Plotting
+    a = get_image(myFile)[0]
+    NUMAX = np.max(a)
+    NUMIN = np.min(a)
+    print("min :", NUMIN, "max :", NUMAX)
 
-size_window = [8, 5]
+    b = a.copy()
+    c = a.copy()
+    d = a.copy()
+    e = a.copy()
 
-fig = plt.figure(figsize = (*size_window,))
+    a_ = a >= (NUMAX-NUMIN)/6
+    b = b >= 2*(NUMAX-NUMIN)/6
+    c = c >= 3*(NUMAX-NUMIN)/6
+    d = d >= 4*(NUMAX-NUMIN)/6
+    e = e >= 5*(NUMAX-NUMIN)/6
 
-fig.add_subplot(253)
-plt.imshow(a, cmap="rainbow")
+    ### Plotting
 
-fig.add_subplot(256)
-plt.imshow(a_, cmap="binary")
+    size_window = [8, 5]
 
-fig.add_subplot(257)
-plt.imshow(b, cmap="binary")
+    fig = plt.figure(figsize = (*size_window,))
 
-fig.add_subplot(258)
-plt.imshow(c, cmap="binary")
+    fig.add_subplot(253)
+    plt.imshow(a, cmap="rainbow")
 
-fig.add_subplot(259)
-plt.imshow(d, cmap="binary")
+    fig.add_subplot(256)
+    plt.imshow(a_, cmap="binary")
 
-fig.add_subplot(2,5,10)
-plt.imshow(e, cmap="binary")
+    fig.add_subplot(257)
+    plt.imshow(b, cmap="binary")
 
-plt.show()
+    fig.add_subplot(258)
+    plt.imshow(c, cmap="binary")
+
+    fig.add_subplot(259)
+    plt.imshow(d, cmap="binary")
+
+    fig.add_subplot(2,5,10)
+    plt.imshow(e, cmap="binary")
+
+    plt.show()
+
+
+parser = ""
+args = ""
+
+def init_args():
+
+    global parser, args
+
+    parser = argparse.ArgumentParser(description='TakeMeOn')
+    parser.add_argument("file", help='file in format FITS or DAT', type=str)
+    args = parser.parse_args()
+
+    args.fantom = True
+
+
+if __name__ == "__main__":
+    init_args()
+    main(args.file)
